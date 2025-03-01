5add85afed71dbafb43938c4da61ed19396b771b
Anshul Chennavaram <anshul@chennavaram.com>
2025-02-28 17:58:26 -0800

    Initial Commit

---
+++ TutoringDudeAI/TutoringDudeAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutoringDudeAIApp.swift
+//  TutoringDudeAI
+//
+//  Created by Anshul Chennavaram on 2/28/25.
+//
+
+import SwiftUI
+
+@main
+struct TutoringDudeAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
