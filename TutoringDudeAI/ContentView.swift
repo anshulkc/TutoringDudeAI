a6f004ca238e9b28dfe66b95d9882d957fb6fc72
Anshul Chennavaram <anshul@chennavaram.com>
2025-02-28 23:59:10 -0800

    Beginning project commit

--- TutoringDudeAI/ContentView.swift
+++ TutoringDudeAI/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    var activities = ["Archery", "Baseball", "Basketball", "Bowling", "Boxing", "Cricket", "Curling", "Fencing", "Golf", "Hiking", "Lacrosse", "Rugby", "Squash"]
+    @State private var selected = "Baseball"
+    var colors: [Color] = [.blue, .cyan, .gray, .green, .indigo, .mint, .orange, .pink, .purple, .red]
+    @State private var id = 1
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("\(selected)!")
+                .font(.largeTitle.bold())
         }
-        .padding()
+        Spacer()
+        VStack {
+            Circle()
+                .fill(colors.randomElement() ?? .blue)
+                .padding()
+                .overlay(
+                    Image(systemName: "figure.\(selected.lowercased())")
+                        .font(.system(size: 144))
+                        .foregroundColor(.white)
+                )
+                .transition(.slide)
+                .id(id)
+            Text("\(selected)")
+                .font(.title)
+        }
+        Spacer()
+        Button("Try again") {
+            withAnimation(.easeInOut(duration: 1)) {
+                selected = activities.randomElement() ?? "Archery"
+                id += 1
+            }
+        }
+        .buttonStyle(.borderedProminent)
     }
 }
 
